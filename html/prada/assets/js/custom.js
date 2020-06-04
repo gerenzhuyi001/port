3d56428919c1377ba7f81bf103391f478bf6e8c4
gerenzhuyi001 <65323155+gerenzhuyi001@users.noreply.github.com>
2020-06-04 11:50:39 +0900

    20200604

--- html/prada/assets/js/custom.js
+++ html/prada/assets/js/custom.js
@@ -1,10 +1,1 @@
-var swiper = new Swiper('#main-banner .main-banner-swiper-container', {
-    scrollbar: {
-      el: '.main-banner-swiper-scrollbar',
-      hide: true,
-    },
-    navigation: {
-        nextEl: '.main-banner-swiper-button-next',
-        prevEl: '.main-banner-swiper-button-prev',
-      },
-  });
+var swiper = new Swiper('#banner .swiper-container');
